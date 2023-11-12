0505af3983a73a92f08d31350ffc8d77b022fa1a
Speeder <ek.speeder@gmail.com>
2023-11-12 18:35:13 +0100

    The initial version of the testable singleton implementation

--- README.md
+++ README.md
@@ -1,2 +1,131 @@
-# testable_singleton
-A C++ singleton class that can easily be unit tested.
+# Testable Singleton
+This header-only C++ library implements the [Singleton pattern](https://en.wikipedia.org/wiki/Singleton_pattern) in a thread-safe and efficient way.
+
+A great issue with singletons normally is that they make unit testing very hard. This library also makes testing relatively easy with additional interfaces that allow resetting the singleton's state to properly test its behaviour. Furthermore, it is also important to be able to test code that depends on singletons, and this implementation lets its users easily inject [mock implementations](https://en.wikipedia.org/wiki/Mock_object) of the singleton. In order to improve code security, the interfaces intended for testing are designed to be unaccessible in production code.
+
+# Usage
+
+To start using this library, include the `singleton.hpp` file into your code, and inherit from the `Singleton` class in your code and get its instance as follows:
+
+```cpp
+class MySingleton : public Singleton<MySingleton>
+{
+private:
+    MySingleton() = default;
+    friend BaseType;
+};
+
+int main()
+{
+    // This returns the constructed singleton.
+    auto& instance = MySingleton::Get();
+
+    // Call functions on `instance` here.
+
+    return 0;
+}
+```
+
+@note It is recommended to define a `private` or `protected` constructor to avoid misuse of the singleton. In this case, the `friend BaseType;` declaration must be added to the body of the class to allow class construction for the singleton.
+
+It is also possible to use a non-default constructor for the singleton class. In this case, initialization arguments must be provided to the `Get()` function at every location where the singleton is accessed, or the `TryGet()` function must be used, which does not initialize the singleton.
+
+```cpp
+#include <singleton.hpp>
+
+class MySingleton : public Singleton<MySingleton>
+{
+private:
+    MySingleton(int customArg1, double customArg2)
+    {
+    }
+    friend BaseType;
+};
+
+int main()
+{
+    {
+        // This returns a nullptr, because the singleton is uninitialized.
+        auto instancePtr = MySingleton::TryGet();
+    }
+    {
+        // This returns the constructed singleton.
+        auto& instance = MySingleton::Get(42, 3.1415);
+    }
+    {
+        // This returns a pointer to the initialized singleton.
+        auto instancePtr = MySingleton::TryGet();
+    }
+    {
+        // This returns the same singleton.
+        // The difference in arguments is ignored.
+        auto& instance = MySingleton::Get(42000, -3.1415);
+    }
+
+    return 0;
+}
+```
+
+For more information about its usage, see the documentation within the [include/singleton.hpp](blob/main/include/singleton.hpp) file.
+
+# Testing
+
+The singleton has private methods which are designed for testing. These methods are normally inaccessible for production code, which is desired for software stability and quality.
+
+The C++ standard states that during explicit template instantiations accessibility checking is not performed. It is possible to exploit this to gain access to private members. Why should we want to get around the accessibility limitation of private members? Because unit tests often need to be able to introspect objects. In the case of this library, the unsafe testing functionality is only accessible by exploiting this behaviour.
+
+While it is complicated and impractical to write the template code to access private members - which is a good thing - there are multiple libraries designed to gain access to this functionality for unit tests. The following example shows the usage of the `Reset()` and `Inject()` methods through the [access_private library](https://github.com/martong/access_private/) by [Gábor Márton](https://github.com/martong).
+
+```cpp
+#include <singleton.hpp>
+#include <access_private.hpp>
+
+class MySingleton : public Singleton<MySingleton>
+{
+protected:
+    // A protected allows a mock to subclass this.
+    MySingleton()
+    {
+    }
+    friend BaseType;
+
+    virtual int MyFunction();
+};
+ACCESS_PRIVATE_STATIC_FUN(MySingleton, MySingleton&(), Reset);
+ACCESS_PRIVATE_STATIC_FUN(MySingleton, void(MySingleton*), Inject);
+
+int main()
+{
+    // This returns the constructed singleton.
+    auto& instance = MySingleton::Get();
+
+    // This returns the real implementation's result.
+    auto realResult1 = instance.MyFunction();
+
+    // This reconstructs the singleton instance and returns the same instance as earlier.
+    auto& instance2 = call_private_static::MySingleton::Reset();
+
+    // This returns the real implementation's result from the fresh instance.
+    auto realResult2 = instance.MyFunction();
+
+    struct MyMockSingleton : public MySingleton
+    {
+        // Override the MyFunction behaviour in the mock.
+        virtual int MyFunction() override;
+    };
+    MyMockSingleton mock;
+
+    // This injects the mock implementation.
+    // The real instance is destroyed, the `instance` and `instance2` variables are invalidated.
+    call_private_static::MySingleton::Inject(&mock);
+
+    // This returns the mock implementation.
+    auto& instance3 = MySingleton::Get();
+
+    return 0;
+}
+```
+
+@note To be able to properly use the `Inject` function, the production code should not cache a reference or pointer to the returned instance. Otherwise the injected mock doesn't take effect and the real instance is used, which is destroyed. This may cause a crash or an other memory corruption style issue.
+
+For more examples and "requirements", it is recommended to view this library's [test code](blob/main/test/unit/singleton_test.cpp).
---
+++ include/singleton.hpp
@@ -0,0 +1,164 @@
+#ifndef TESTABLE_SINGLETON_INCLUDED_H
+#define TESTABLE_SINGLETON_INCLUDED_H
+
+#include <mutex>
+#include <utility>
+
+/// Implements the singleton pattern, but makes unit testing easy.
+/** To use the Singleton class normally, inherit from it with the CRTP style, like:
+  *
+  * ```cpp
+  * class MyClass : public Singleton<MyClass> { impl... };
+  * ```
+  *
+  * After this, external code is able to get the `MyClass` instance by using `MyClass::Get()`.
+  *
+  * To test your singleton, an access-private library implementation is required, such as
+  * <https://github.com/martong/access_private>. Using the access-private library, invoke the
+  * `MyClass::Inject()` function to inject a mock implementation into the singleton. It is also
+  * possible to reconstruct the Singleton with different constructor arguments by using the
+  * `MyClass::Reset()` function.
+  * 
+  * @remark The `Inject()` and `Reset()` functions are NOT thread safe! They should only be used
+  *         in test code sections while implementation code is not running on a different thread.
+  */
+template <typename T>
+struct Singleton
+{
+    using BaseType = Singleton<T>;
+
+    /// Returns the instance of the class.
+    /** @remark The constructor arguments are only used if the instance is not constructed yet.
+      */
+    template <typename ...Args>
+    static T& Get(Args... args)
+    {
+        std::call_once(g_onceFlag, [](Args... args) {
+                g_instance.Emplace(std::forward<Args>(args)...);
+            }, args...);
+        return *static_cast<T*>(g_instance);
+    }
+
+    ///  Returns the instance of the class without construction.
+    /** @return The instance of the singleton or a `nullptr` if unconstructed.
+      * @remark This can be useful for singletons that have custom constructor arguments. Code can
+      *         get an already initialied instance, if it has been initialized already, without
+      *         having to specify the arguments.
+      */
+    static T* TryGet()
+    {
+        return g_instance;
+    }
+
+protected:
+    Singleton() noexcept = default;
+private:
+    Singleton(const Singleton&) = delete;
+    Singleton& operator =(const Singleton&) = delete;
+
+    /// Holds the instance of T, either locally constructed or injected.
+    static struct Instance final
+    {
+        Instance() noexcept = default;
+        Instance(const Instance&) = delete;
+        ~Instance()
+        {
+            // Destroys the locally-initialized instance. Injected ones are ignored (no ownership).
+            if (m_pExtern == LOCAL_INSTANCE_ID)
+                GetBuffer().~T();
+        }
+        Instance& operator =(const Instance&) = delete;
+        /// Returns the current instance.
+        operator T* () { return m_pExtern == LOCAL_INSTANCE_ID ? &GetBuffer() : m_pExtern; }
+        /// Constructs the singleton within the local buffer.
+        template <typename ...Args>
+        void Emplace(Args... args)
+        {
+            this->~Instance();
+            new (&GetBuffer()) T(std::forward<Args>(args)...);
+            m_pExtern = LOCAL_INSTANCE_ID;
+        }
+        /// Sets an external object as the instance.
+        /** @remark If `ptr` is `nullptr`, this is just reset.
+          */
+        void SetExtern(T* ptr)
+        {
+            this->~Instance();
+            m_pExtern = ptr;
+        }
+    private:
+        /// A special pointer value to specify that the local buffer is constructed.
+        static T* const LOCAL_INSTANCE_ID;
+        /// nullptr if empty; LOCAL_INSTANCE_ID if using internal buffer;
+        /// pointer to external object otherwise.
+        T* m_pExtern = nullptr;
+        /// Returns an (uninitialized) internal buffer for storing T.
+        static T& GetBuffer()
+        {
+            // Static, uninitialized buffer for the singleton's object
+            static union U { T asT; U(){} ~U(){} } buffer;
+            return buffer.asT;
+        }
+    } g_instance;
+
+    /// Holds a flag used for `std::call_once()`.
+    static struct OnceFlag final
+    {
+        OnceFlag() noexcept
+        {
+            new (&buffer.asOnceFlag) std::once_flag();
+        }
+        OnceFlag(const OnceFlag&) = delete;
+        ~OnceFlag()
+        {
+            buffer.asOnceFlag.~once_flag();
+        }
+        OnceFlag& operator =(const OnceFlag&) = delete;
+        /// Resets the OnceFlag to initial state (allowing another call)
+        void Reset()
+        {
+            this->~OnceFlag();
+            new (this) OnceFlag();
+        }
+        /// Returns the internal std::once_flag. May be uninitialized.
+        operator std::once_flag& ()
+        {
+            return buffer.asOnceFlag;
+        }
+    private:
+        union U { std::once_flag asOnceFlag; U(){} ~U(){} } buffer;
+    } g_onceFlag;
+
+    /// (Re)constructs the internal singleton instance.
+    /** If an existing singleton instance was already constructed, it is destroyed. If an external
+      * instance was injected, it is overridden with the newly constructed instance.
+      * 
+      * @remark This function is not thread safe. It is intended for tests, not production code.
+      */
+    template <typename ...Args>
+    static T& Reset(Args... args)
+    {
+        g_onceFlag.Reset();
+        return Get(std::forward<Args>(args)...);
+    }
+
+    /// Injects an external instance into the singleton.
+    /** If an external instance is injected, the `Get()` function 
+      * @param object The object is taken without ownership and must be deleted by the caller.
+      * @remark If `object` is `nullptr`, it resets the singleton to uninitialized state, and the
+      *         next invocation of `Get()` reconstructs the instance.
+      */
+    static void Inject(T* object)
+    {
+        std::call_once(g_onceFlag, []() {});
+        g_instance.SetExtern(object);
+    }
+};
+template <typename T>
+typename Singleton<T>::Instance Singleton<T>::g_instance;
+template <typename T>
+typename Singleton<T>::OnceFlag Singleton<T>::g_onceFlag;
+template <typename T>
+T* const Singleton<T>::Instance::LOCAL_INSTANCE_ID = reinterpret_cast<T*>(1);
+
+#endif
