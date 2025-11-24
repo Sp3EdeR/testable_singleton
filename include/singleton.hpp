3324e3d796f36aea6bb4015b7c0459b438fdb3cd
benjamin.varju <benjamin.varju@rollingwireless.com>
2025-11-24 12:17:33 +0100

    fix: format

--- include/singleton.hpp
+++ include/singleton.hpp
@@ -6,8 +6,6 @@
 #include <type_traits>
 #include <utility>
 
-/// This file implements the singleton pattern, but makes unit testing easy.
-
 // More effort would be required to make load-time singletons work on MSVC
 #ifndef ENABLE_LOAD_TIME_SINGLETON
 #if defined(__GNUC__) || defined(__clang__)
@@ -15,7 +13,7 @@
 #else
 #define ENABLE_LOAD_TIME_SINGLETON 0
 #endif
-#endif // ENABLE_LOAD_TIME_SINGLETON
+#endif
 
 /**
  * @brief Types of singletons
@@ -48,100 +46,11 @@ enum class SingletonType : int32_t {
 #endif // ENABLE_LOAD_TIME_SINGLETON
 };
 
-// fw declare some dependencies
-namespace Detail {
-template <typename T, SingletonType instanceType> struct SingletonInstance;
-}
-
-/** To use the Singleton class normally, inherit from it with the CRTP style, like:
- *
- * ```cpp
- * class MyClass : public Singleton<MyClass> { impl... };
- * ```
- *
- * After this, external code is able to get the `MyClass` instance by using `MyClass::Get()`.
- *
- * To test your singleton, an access-private library implementation is required, such as
- * <https://github.com/martong/access_private>. Using the access-private library, invoke the
- * `MyClass::Inject()` function to inject a mock implementation into the singleton. It is also
- * possible to reconstruct the Singleton with different constructor arguments by using the
- * `MyClass::Reset()` function.
- *
- * @remark The `Inject()` and `Reset()` functions are NOT thread safe! They should only be used
- *         in test code sections while implementation code is not running on a different thread.
- */
-template <typename T, SingletonType type = SingletonType::STATIC> struct Singleton {
-    using BaseType = Singleton<T, type>;
-
-    /// Returns the instance of the class.
-    /** @remark The constructor arguments are only used if the instance is not constructed yet.
-     */
-    template <typename... Args> static T& Get(Args&&... args);
-
-    ///  Returns the instance of the class without construction.
-    /** @return The instance of the singleton or a `nullptr` if unconstructed.
-     * @remark This can be useful for singletons that have custom constructor arguments. Code can
-     *         get an already initialied instance, if it has been initialized already, without
-     *         having to specify the arguments.
-     */
-    static T* TryGet();
-
-protected:
-    Singleton() noexcept = default;
-
-private:
-    using Instance = Detail::SingletonInstance<T, type>;
-#if ENABLE_LOAD_TIME_SINGLETON
-    // just here to catch future regressions
-    static_assert(type != SingletonType::LOAD_TIME || std::is_trivially_destructible_v<Instance>,
-        "Load-time singletons must be trivially destructible");
-#endif // ENABLE_LOAD_TIME_SINGLETON
-
-    Singleton(const Singleton&) = delete;
-    Singleton& operator=(const Singleton&) = delete;
-
-    static Instance g_instance;
-
-    /// Holds a flag used for `std::call_once()`.
-    struct OnceFlag final {
-        OnceFlag() noexcept;
-        OnceFlag(const OnceFlag&) = delete;
-        ~OnceFlag();
-        OnceFlag& operator=(const OnceFlag&) = delete;
-        /// Resets the OnceFlag to initial state (allowing another call)
-        void Reset();
-        /// Returns the internal std::once_flag. May be uninitialized.
-        operator std::once_flag&();
-
-    private:
-        union U {
-            std::once_flag asOnceFlag;
-            U() { }
-            ~U() { }
-        } buffer;
-    };
-
-    static OnceFlag g_onceFlag;
-
-    /// (Re)constructs the internal singleton instance.
-    /** If an existing singleton instance was already constructed, it is destroyed. If an external
-     * instance was injected, it is overridden with the newly constructed instance.
-     *
-     * @remark This function is not thread safe. It is intended for tests, not production code.
-     */
-    template <typename... Args> static T& Reset(Args&&... args);
-
-    /// Injects an external instance into the singleton.
-    /** If an external instance is injected, the `Get()` function
-     * @param object The object is taken without ownership and must be deleted by the caller.
-     * @remark If `object` is `nullptr`, it resets the singleton to uninitialized state, and the
-     *         next invocation of `Get()` reconstructs the instance.
-     */
-    static void Inject(T* object);
-};
-
 // Specialiaze some details of the singleton class in "private" Detail namespace
 namespace Detail {
+
+template <typename T, SingletonType instanceType>
+struct SingletonInstance;
 
 // Static lifetime singleton specialization (default)
 template <typename T> struct SingletonInstance<T, SingletonType::STATIC> {
@@ -205,13 +114,13 @@ T* const SingletonInstance<T, SingletonType::STATIC>::LOCAL_INSTANCE_ID = reinte
 // are called at "unload" time.
 
 using DeleteLoadTimeSingletonFunc = void (*)();
-
 struct LoadTimeSingletonEntry {
     bool m_isValid = true; // To delete or not (to avoid double free between Reset and __attribute__(destructor))
     DeleteLoadTimeSingletonFunc m_deleteFunc;
     LoadTimeSingletonEntry* m_next;
 };
-// note to maintainer: all types used by load time singletons must be trivially destructible,
+// note to maintainer: all types that have static storage
+// used by load time singletons must be trivially destructible,
 // otherwise they will get destroyed during static destruction phase before "unload" time.
 static_assert(std::is_trivially_destructible_v<LoadTimeSingletonEntry>);
 
@@ -302,53 +211,126 @@ LoadTimeSingletonEntry SingletonInstance<T, SingletonType::LOAD_TIME>::g_entry
     = { false, SingletonInstance<T, SingletonType::LOAD_TIME>::DestroyInternalInstance, nullptr };
 #endif // ENABLE_LOAD_TIME_SINGLETON
 
-} // namespace Detail
+}
 
-// Implementation of Singleton class
+/// Implements the singleton pattern, but makes unit testing easy.
+/** To use the Singleton class normally, inherit from it with the CRTP style, like:
+  *
+  * ```cpp
+  * class MyClass : public Singleton<MyClass> { impl... };
+  * ```
+  *
+  * After this, external code is able to get the `MyClass` instance by using `MyClass::Get()`.
+  *
+  * To test your singleton, an access-private library implementation is required, such as
+  * <https://github.com/martong/access_private>. Using the access-private library, invoke the
+  * `MyClass::Inject()` function to inject a mock implementation into the singleton. It is also
+  * possible to reconstruct the Singleton with different constructor arguments by using the
+  * `MyClass::Reset()` function.
+  *
+  * @remark The `Inject()` and `Reset()` functions are NOT thread safe! They should only be used
+  *         in test code sections while implementation code is not running on a different thread.
+  */
+template <typename T, SingletonType type = SingletonType::STATIC>
+struct Singleton
+{
+    using BaseType = Singleton<T, type>;
 
-template <typename T, SingletonType type> template <typename... Args> T& Singleton<T, type>::Get(Args&&... args)
-{
-    std::call_once(
-        g_onceFlag, [](Args&&... args) { g_instance.Emplace(std::forward<Args>(args)...); },
-        std::forward<Args>(args)...);
-    return *static_cast<T*>(g_instance);
-}
-template <typename T, SingletonType type> T* Singleton<T, type>::TryGet()
-{
-    return g_instance;
-}
-template <typename T, SingletonType type> template <typename... Args> T& Singleton<T, type>::Reset(Args&&... args)
-{
-    g_onceFlag.Reset();
-    return Get(std::forward<Args>(args)...);
-}
-template <typename T, SingletonType type> void Singleton<T, type>::Inject(T* object)
-{
-    if (object)
-        std::call_once(g_onceFlag, []() {});
-    else
+    /// Returns the instance of the class.
+    /** @remark The constructor arguments are only used if the instance is not constructed yet.
+      */
+    template <typename ...Args>
+    static T& Get(Args... args)
+    {
+        std::call_once(g_onceFlag, [](Args... args) {
+                g_instance.Emplace(std::forward<Args>(args)...);
+            }, args...);
+        return *static_cast<T*>(g_instance);
+    }
+
+    ///  Returns the instance of the class without construction.
+    /** @return The instance of the singleton or a `nullptr` if unconstructed.
+      * @remark This can be useful for singletons that have custom constructor arguments. Code can
+      *         get an already initialied instance, if it has been initialized already, without
+      *         having to specify the arguments.
+      */
+    static T* TryGet()
+    {
+        return g_instance;
+    }
+
+protected:
+    Singleton() noexcept = default;
+private:
+    using Instance = Detail::SingletonInstance<T, type>;
+#if ENABLE_LOAD_TIME_SINGLETON
+    // just here to catch future regressions
+    static_assert(type != SingletonType::LOAD_TIME || std::is_trivially_destructible_v<Instance>,
+        "Load-time singletons must be trivially destructible");
+#endif
+
+    Singleton(const Singleton&) = delete;
+    Singleton& operator =(const Singleton&) = delete;
+
+    static Instance g_instance;
+
+    /// Holds a flag used for `std::call_once()`.
+    static struct OnceFlag final
+    {
+        OnceFlag() noexcept
+        {
+            new (&buffer.asOnceFlag) std::once_flag();
+        }
+        OnceFlag(const OnceFlag&) = delete;
+        ~OnceFlag()
+        {
+            buffer.asOnceFlag.~once_flag();
+        }
+        OnceFlag& operator =(const OnceFlag&) = delete;
+        /// Resets the OnceFlag to initial state (allowing another call)
+        void Reset()
+        {
+            this->~OnceFlag();
+            new (this) OnceFlag();
+        }
+        /// Returns the internal std::once_flag. May be uninitialized.
+        operator std::once_flag& ()
+        {
+            return buffer.asOnceFlag;
+        }
+    private:
+        union U { std::once_flag asOnceFlag; U(){} ~U(){} } buffer;
+    } g_onceFlag;
+
+    /// (Re)constructs the internal singleton instance.
+    /** If an existing singleton instance was already constructed, it is destroyed. If an external
+      * instance was injected, it is overridden with the newly constructed instance.
+      *
+      * @remark This function is not thread safe. It is intended for tests, not production code.
+      */
+    template <typename ...Args>
+    static T& Reset(Args... args)
+    {
         g_onceFlag.Reset();
-    g_instance.SetExtern(object);
-}
-template <typename T, SingletonType type> Singleton<T, type>::OnceFlag::OnceFlag() noexcept
-{
-    new (&buffer.asOnceFlag) std::once_flag();
-}
-template <typename T, SingletonType type> Singleton<T, type>::OnceFlag::~OnceFlag()
-{
-    buffer.asOnceFlag.~once_flag();
-}
-template <typename T, SingletonType type> void Singleton<T, type>::OnceFlag::Reset()
-{
-    this->~OnceFlag();
-    new (this) OnceFlag();
-}
-template <typename T, SingletonType type> Singleton<T, type>::OnceFlag::operator std::once_flag&()
-{
-    return buffer.asOnceFlag;
-}
+        return Get(std::forward<Args>(args)...);
+    }
 
-template <typename T, SingletonType type> typename Singleton<T, type>::OnceFlag Singleton<T, type>::g_onceFlag;
-template <typename T, SingletonType type> typename Singleton<T, type>::Instance Singleton<T, type>::g_instance;
-
-#endif // TESTABLE_SINGLETON_INCLUDED_H
+    /// Injects an external instance into the singleton.
+    /** If an external instance is injected, the `Get()` function
+      * @param object The object is taken without ownership and must be deleted by the caller.
+      * @remark If `object` is `nullptr`, it resets the singleton to uninitialized state, and the
+      *         next invocation of `Get()` reconstructs the instance.
+      */
+    static void Inject(T* object)
+    {
+        if (object)
+            std::call_once(g_onceFlag, []() {});
+        else
+            g_onceFlag.Reset();
+        g_instance.SetExtern(object);
+    }
+};
+template <typename T, SingletonType type>
+typename Singleton<T, type>::OnceFlag Singleton<T, type>::g_onceFlag;
+template <typename T, SingletonType type>
+typename Singleton<T, type>::Instance Singleton<T, type>::g_instance;
